ae4066baa9b548de1a4f2e221e54de87a1082a73
Egidijus Adzgauskas <eadzgauskas@gmail.com>
2021-09-27 19:13:50 +0300

    demo
    testing json to js

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict"
+console.log('veikia');
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("/portfolio.json", function(text){
+    var data = JSON.parse(text);
+    
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i].name);
+        console.log(data[i].description);
+        console.log(data[i].link);
+
+      }
+    
+});
+
+//
